21aa0043bf992cbcfe29b157ee8b3ee07a9de19e
Efeglas <gabor.atty@gmail.com>
2023-03-21 21:29:29 +0100

    New routes, toasty
    Added new route definitions
    Added toasty package fot notifications

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { Fragment, useEffect, useState } from "react";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Login from "./pages/Login";
 import Root from "./pages/Root";
 import ErrorPage from "./pages/ErrorPage";
 import PrivateRoute from "./pages/PrivateRoute";
 import Search from "./pages/Search";
+import Roles from "./pages/Roles";
+import Users from "./pages/Users";
+import Password from "./pages/Password";
+import Profile, {loader as userDataLoader} from "./pages/Profile";
+import UserInfo, {loader as adminsLoader} from "./pages/UserInfo";
 
 const router = createBrowserRouter([
   {path: "/login", element: <Login />},
+  {path: "/userInfo", element: <UserInfo />, loader: adminsLoader},
   {path: "/", element: <PrivateRoute to='/login'><Root /></PrivateRoute>, errorElement: <ErrorPage />, children: [
     {path: "/", element: <Search />}, 
+    {path: "/roles", element: <Roles />},
+    {path: "/users", element: <Users />},
+    {path: "/password", element: <Password />},
+    {path: "/profile", element: <Profile />, loader: userDataLoader},
   ]},
 ]);
 
@@ -18,7 +31,7 @@ function App () {
 
   const [userIsLoggedIn, setUserIsLoggedIn] = useState(false);
 
-  useEffect(async () => {
+  useEffect(() => {
     const token = localStorage.getItem('token');
     if (token === null) {
       setUserIsLoggedIn(false);
@@ -26,20 +39,13 @@ function App () {
       setUserIsLoggedIn(true);
     }
 
-    const response = await fetch("http://192.168.50.62:8080/user/test", {
-              method: "GET",
-              headers: {
-                  'Content-Type': 'application/json',
-              },            
-          });
-          const json = await response.json();
-          console.log(json); 
   }, []);
 
 
   return (
     <Fragment>
       <RouterProvider router={router} />
+      <ToastContainer />
     </Fragment>
   );
 }
