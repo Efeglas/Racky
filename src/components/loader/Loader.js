882c874e747bc57b5fffabcb415f29a8cb11ab75
Efeglas <gabor.atty@gmail.com>
2023-03-22 21:57:48 +0100

    Added portal to modal

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,11 +1,20 @@
+import { Fragment } from 'react';
 import Icon from '../../icons/Icon';
 import style from './Loader.module.css';
+import ReactDOM from 'react-dom';
 
+
+    
 const Loader = () => {
+
+    const portalElement = document.getElementById("overlays");
+
     return (
-        <div className={style.loader}>
-            <Icon size='50' icon='spinner'/>
-        </div>
+        <Fragment>
+            {ReactDOM.createPortal(<div className={style.loader}>
+                <Icon size='50' icon='spinner'/>
+            </div>, portalElement)}
+        </Fragment>
     );
 }
 export default Loader;
